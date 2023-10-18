0086231ef9f7cb7b59ef04cc3914cbafc56aa559
justinknguyen <justinknguyen11@gmail.com>
2023-10-18 14:48:47 -0600

    Deleted frontend FetchData and Counter template files and code
    - Removed navbar links and routes

--- EmployeeTable/EmployeeTable/ClientApp/src/AppRoutes.js
+++ EmployeeTable/EmployeeTable/ClientApp/src/AppRoutes.js
@@ -6,14 +6,6 @@ const AppRoutes = [
   {
     index: true,
     element: <Home />
-  },
-  {
-    path: '/counter',
-    element: <Counter />
-  },
-  {
-    path: '/fetch-data',
-    element: <FetchData />
   }
 ];
 
