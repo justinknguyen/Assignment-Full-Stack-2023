6f7d418ce45fbe7128fffbd4be1235c1050ae351
justinknguyen <justinknguyen11@gmail.com>
2023-10-18 14:17:19 -0600

    Created EmployeesController and deleted Weather templates
    - Created controller class to handle incoming HTTP requests
    - Added GetSync(string id) for searching a specific employee to update information
    - Deleted generated Weather templates

---
+++ EmployeeTable/EmployeeTable/Controllers/EmployeesController.cs
@@ -0,0 +1,67 @@
+﻿using EmployeeTable.Models;
+using EmployeeTable.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeTable.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly EmployeesService _employeesService;
+
+        public EmployeesController(EmployeesService employeesService)
+        {
+            _employeesService = employeesService;
+        }
+
+        [HttpGet]
+        public async Task<List<Employee>> Get()
+        {
+            return await _employeesService.GetAsync();
+        }
+            
+        [HttpGet("{id:length(24)}")]
+        public async Task<ActionResult<Employee>> Get(string id)
+        {
+            var employee = await _employeesService.GetAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return employee;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Employee newEmployee)
+        {
+            await _employeesService.CreateAsync(newEmployee);
+            return CreatedAtAction(nameof(Get), new { id = newEmployee.Id }, newEmployee);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public async Task<IActionResult> Update(string id, Employee updatedEmployee)
+        {
+            var employee = await _employeesService.GetAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            updatedEmployee.Id = employee.Id;
+            await _employeesService.UpdateAsync(id, updatedEmployee);
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var employee = await _employeesService.GetAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            await _employeesService.RemoveAsync(id);
+            return NoContent();
+        }
+    }
+}
--- EmployeeTable/EmployeeTable/Services/EmployeesService.cs
+++ EmployeeTable/EmployeeTable/Services/EmployeesService.cs
@@ -11,14 +11,15 @@ namespace EmployeeTable.Services
         public EmployeesService(IOptions<CompanyDatabaseSettings> companyDatabaseSettings)
         {
             var mongoClient = new MongoClient(companyDatabaseSettings.Value.ConnectionString);
-
             var mongoDatabase = mongoClient.GetDatabase(companyDatabaseSettings.Value.DatabaseName);
-
             _employeesCollection = mongoDatabase.GetCollection<Employee>(companyDatabaseSettings.Value.CollectionName);
         }
 
         public async Task<List<Employee>> GetAsync() =>
             await _employeesCollection.Find(_ => true).ToListAsync();
+
+        public async Task<Employee?> GetAsync(string id) =>
+            await _employeesCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
 
         public async Task CreateAsync(Employee newEmployee) =>
             await _employeesCollection.InsertOneAsync(newEmployee);
