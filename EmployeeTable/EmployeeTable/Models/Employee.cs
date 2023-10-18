eadad7b70f6c06e4ec1ef1c6cd0c51ddb3310f5c
justinknguyen <justinknguyen11@gmail.com>
2023-10-18 13:22:45 -0600

    Created Employee model and installed the MongoDB driver

---
+++ EmployeeTable/EmployeeTable/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace EmployeeTable.Models
+{
+    public class Employee
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("firstName")]
+        public string FirstName { get; set; } = null!;
+
+        [BsonElement("lastName")]
+        public string LastName { get; set;} = null!;
+
+        [BsonElement("salary")]
+        public int Salary { get; set; }
+    }
+}
