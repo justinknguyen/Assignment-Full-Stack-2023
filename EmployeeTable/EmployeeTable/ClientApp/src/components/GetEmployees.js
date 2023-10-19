b301a12c547032f1479e2d989ff17ddd6021784f
justinknguyen <justinknguyen11@gmail.com>
2023-10-18 19:26:35 -0600

    Added MaterialUI table and implemented Delete button
    - Initial table displaying employees, with Edit and Delete buttons in each row

--- EmployeeTable/EmployeeTable/ClientApp/src/components/GetEmployees.js
+++ EmployeeTable/EmployeeTable/ClientApp/src/components/GetEmployees.js
@@ -1,10 +1,42 @@
 import { useState, useEffect } from 'react';
+import Button from '@mui/material/Button';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 /**
  * Get and list all employees in the database.
  */
 const GetEmployees = () => {
     const [employees, setEmployees] = useState([]);
+
+    function handleEdit(employee) {
+        console.log(employee);
+    }
+
+    // Remove the employee based on Id
+    function handleDelete(employee) {
+        fetch(`api/employees/${employee.id}`, {
+            method: 'DELETE'
+        })
+            .then(res => {
+                if (res.ok) {
+                    // Employee removed
+                    console.log('Removed the employee successfully!');
+                    setEmployees(employees.filter(emp => emp.id !== employee.id));
+                } else {
+                    // Employee could not be removed
+                    console.error('Error removing the employee.');
+                }
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    }
 
     // Fetch all employees on page load
     useEffect(() => {
@@ -17,17 +49,34 @@ const GetEmployees = () => {
     return (
         <>
             <h1>Employees</h1>
-            <ul>
-                {/* List the fetched employees */}
-                {employees.map(emp => (
-                    <li key={emp.id}>
-                        <p><strong>Id:</strong> {emp.id}</p>
-                        <p><strong>First Name:</strong> {emp.firstName}</p>
-                        <p><strong>Last Name:</strong> {emp.lastName}</p>
-                        <p><strong>Salary:</strong> ${emp.salary.toLocaleString()}</p>
-                    </li>
-                ))}
-            </ul>
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                    <TableHead>
+                        <TableRow className='table-header'>
+                            <TableCell>First Name</TableCell>
+                            <TableCell align="right">Last Name</TableCell>
+                            <TableCell align="right">Salary</TableCell>
+                            <TableCell align="right">Actions</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {employees.map((employee) => (
+                            <TableRow
+                                key={employee.id}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            >
+                                <TableCell component="th" scope="row">{employee.firstName}</TableCell>
+                                <TableCell align="right">{employee.lastName}</TableCell>
+                                <TableCell align="right"> ${employee.salary.toLocaleString()}</TableCell>
+                                <TableCell align="right">
+                                    <Button variant="text" onClick={() => handleEdit(employee)}>Edit</Button>
+                                    <Button variant="text" onClick={() => handleDelete(employee)}>Delete</Button>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </>
     );
 }
